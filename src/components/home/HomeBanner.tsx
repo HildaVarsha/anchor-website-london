c7f895e77d5c7fc6261574d6b214affd74cef34a
HildaReplika <hilda@replikatech.com>
2024-11-05 18:37:20 +0530

    home banner ui done

---
+++ src/components/home/HomeBanner.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "../ui";
+
+const HomeBanner = () => {
+  return (
+    <div
+      className="flex flex-col justify-center"
+      style={{
+        backgroundImage: "url('/banner.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div className="container mx-auto text-stone-700">
+        <p className="text-5xl font-bold">We Drive Digital</p>
+        <p className="text-5xl font-bold py-4">Transformation, Globally.</p>
+        <Button>View More</Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
