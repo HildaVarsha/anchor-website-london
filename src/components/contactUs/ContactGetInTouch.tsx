b6ca4456daa36a0eb394f69b6e92b242a7824e6a
HildaReplika <hilda@replikatech.com>
2024-11-08 08:51:03 +0530

    contact us ui done

---
+++ src/components/contactUs/ContactGetInTouch.tsx
@@ -0,0 +1,133 @@
+import React from "react";
+import { Button, Card, Input, Textarea } from "../ui";
+import { PageHeader } from "../shared";
+import Link from "next/link";
+import Image from "next/image";
+
+const ContactGetInTouch = () => {
+  return (
+    <div className="py-12 px-4 md:px-0 bg-blue-950 w-full h-full md:h-[800px]">
+      <p className="text-sm text-white font-semibold text-center pb-2">
+        Get In Touch
+      </p>
+      <PageHeader label="Hey!Lets Talk" className="text-white text-center" />
+      <div className="container mx-auto flex flex-col md:flex-row items-stretch justify-between gap-12 h-full">
+        <div className="w-full md:w-[60%] md:h-full">
+          <Card className="rounded-md p-8 h-[500px]">
+            <Input placeholder="Name" className="mb-4 h-12" />
+            <Input placeholder="Email" className="mb-4 h-12" />
+            <Input placeholder="Phone" className="mb-4 h-12" />
+            <Textarea placeholder="Your Message" className="mb-4 h-44" />
+            <Button className="mt-4">Send Now</Button>
+          </Card>
+        </div>
+        <div className="w-full md:w-[40%]">
+          <Card className="rounded-md p-8">
+            <div className="flex flex-col md:flex-row  text-center md:text-start items-center gap-8 mb-8">
+              <Button className="h-20 w-20 bg-stone-100 text-blue-600 ">
+                <Image
+                  src={"/contact-call.png"}
+                  alt="Contact Us"
+                  width={32}
+                  height={32}
+                />
+              </Button>
+              <div>
+                <p className="font-bold text-xl text-blue-900 pb-1">
+                  Call Anytime
+                </p>
+                <Link href={"tel:+447438846089"}>Mobile:+44 7438 846089</Link>
+                <div>
+                  <Link href={"tel:+02034909639"}>Landline:+020 3490 9639</Link>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row  text-center md:text-start items-center gap-8 mb-8">
+              <Button className="h-20 w-20 bg-stone-100 text-blue-600 ">
+                <Image
+                  src={"/contact-email.png"}
+                  alt="Contact Us"
+                  width={32}
+                  height={32}
+                />
+              </Button>
+              <div>
+                <p className="font-bold text-xl text-blue-900 pb-1">
+                  Send Email
+                </p>
+                <Link href={"mailto:rafi@anchorinformatics.co.uk"}>
+                  Email:rafi@anchorinformatics.co.uk
+                </Link>
+                <div>
+                  <Link href={"mailto:contact@anchorinformatics.co.uk"}>
+                    Enquiry:contact@anchorinformatics.co.uk
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row  text-center md:text-start items-center gap-8 mb-8">
+              <Button className="h-20 w-28 bg-stone-100 text-blue-600 ">
+                <Image
+                  src={"/contact-map.png"}
+                  alt="Contact Us"
+                  width={32}
+                  height={32}
+                />
+              </Button>
+              <div>
+                <p className="font-bold text-xl text-blue-900 pb-1">Visit Us</p>
+                <Link href={"https://maps.app.goo.gl/FtDNDopyrnDSwoJWA"}>
+                  Address:184 Heigham Road,London E62JH U.K.
+                </Link>
+                <div>
+                  <Link href={"https://maps.app.goo.gl/gEMC5kPbprGZJDT1A"}>
+                    Address:Vaishnavis
+                    Cynosure,Gachibowli,Hyderabad,Telangana-500032
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+                <Button className="w-16 h-16">
+                  <Image
+                    src={"/linkedIn.png"}
+                    alt="Linked In"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+                <Button className="w-16 h-16">
+                  <Image
+                    src={"/instagram.png"}
+                    alt="Instagram"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+                <Button className="w-16 h-16">
+                  <Image
+                    src={"/facebook.png"}
+                    alt="Facebook"
+                    width={20}
+                    height={20}
+                  />
+                </Button>
+                <Button className="w-16 h-16">
+                  <Image
+                    src={"/twitter-contact.png"}
+                    alt="Twitter"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactGetInTouch;
